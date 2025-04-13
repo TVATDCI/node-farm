54059ffff89f0fc938d6694e15bdde35dbf36de6
TVATDCI <tvatdci@gmail.com>
2025-04-13 18:40:14 +0200

    create output.txt

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const test = "Test";
-const node = "Node Version";
-console.log(test + " " + node);
+// CommonJs recap
+const fs = require("fs");
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `Do you know? \n${textIn}. \nCreated on ${Date.now()}`;
+fs.writeFileSync(`./txt/output.text`, textOut);
+console.log(`File written`);
