1ade47dbd83c8f5c7641175c1f4dd8becfefb07d
TVATDCI <tvatdci@gmail.com>
2025-04-14 20:31:01 +0200

    data1 to data2

--- index.js
+++ index.js
@@ -35,8 +35,10 @@ const fs = require("fs");
      */
 }
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  console.log(data);
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+  });
 });
 console.log(
   `Node.js will read data -read-this- in the background. \nSo it will read this line first and then the data below`
