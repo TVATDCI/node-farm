6bf4152707600971051ce45a72b650317e932c53
TVATDCI <tvatdci@gmail.com>
2025-04-15 14:40:53 +0200

    More comment added

--- index.js
+++ index.js
@@ -105,14 +105,14 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
     console.log(data2);
     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+    //   console.log(data3);
 
       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
         console.log("Non-blocking Asynchronous has been built 🚀 ");
       });
     });
   });
-});
+// });
 
 console.log(
   `Node.js will read data -read-this- in the background. \nSo it will read this line first and then the data below`
