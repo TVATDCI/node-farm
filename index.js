561b05969934181493fdee658a45b6fb9b80668f
TVATDCI <tvatdci@gmail.com>
2025-04-20 01:17:21 +0200

    clean-up index.js

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to request on port 8000 💆‍♀️");
+  console.log("feature/modules - Listening to request on port 8000 💆‍♀️");
 });
