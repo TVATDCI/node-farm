8ea0d4f2b02d89610215f4cd5280de23c9c9a5c4
TVATDCI <tvatdci@gmail.com>
2025-04-20 08:39:24 +0200

    removed fn replaceTemplate to modules/replaceTemplate.js

--- index.js
+++ index.js
@@ -2,25 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-const replaceTemplate = (temp, product) => {
-  let productOutput = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  productOutput = productOutput.replace(/{%IMAGE%}/g, product.image);
-  productOutput = productOutput.replace(/{%FROM%}/g, product.from);
-  productOutput = productOutput.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  productOutput = productOutput.replace(/{%QUANTITY%}/g, product.quantity);
-  productOutput = productOutput.replace(/{%PRICE%}/g, product.price);
-  productOutput = productOutput.replace(
-    /{%DESCRIPTION%}/g,
-    product.description
-  );
-  productOutput = productOutput.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    productOutput = productOutput.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return productOutput;
-};
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
