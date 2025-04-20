58eae95808a614e5a2ee6401fc4d3a689d147b3e
TVATDCI <tvatdci@gmail.com>
2025-04-20 08:36:55 +0200

    implemented module.exports(commonJs)

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,2 +1,19 @@
-// The plan is to modularize things, and make the codebase nice and clean.
-// But first — break time, stretch, hydrate and chill out a bit. 👌
+module.exports = (temp, product) => {
+  let productOutput = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  productOutput = productOutput.replace(/{%IMAGE%}/g, product.image);
+  productOutput = productOutput.replace(/{%FROM%}/g, product.from);
+  productOutput = productOutput.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  productOutput = productOutput.replace(/{%QUANTITY%}/g, product.quantity);
+  productOutput = productOutput.replace(/{%PRICE%}/g, product.price);
+  productOutput = productOutput.replace(
+    /{%DESCRIPTION%}/g,
+    product.description
+  );
+  productOutput = productOutput.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    productOutput = productOutput.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return productOutput;
+};
