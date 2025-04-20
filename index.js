4fcef8b49903351a16f656cdebbfcaaf27b25ffa
TNTHNGVDYNND <tuanthong.vaidyanond@gmail.com>
2025-04-20 19:42:01 +0200

    adjusted index.js

--- index.js
+++ index.js
@@ -52,5 +52,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("feature/modules - Listening to request on port 8000 💆‍♀️");
+  console.log("Listening to request on port 8000 💆‍♀️");
 });
